307639f0cbd74e20e5d0504d7e839a3232718075
Pedro Lacerda <pedrohrl1@gmail.com>
2023-12-20 17:54:46 -0300

    Revert "add first js file"
    This reverts commit 8f461f6ff884dab913ba9d88cfcc7756a1e8b166.

--- 01. sanitize/index.js
+++
@@ -1,6 +0,0 @@
-/* Função que sanitiza uma string para evitar cross-site scripting */
-function sanitize(string) {
-  return string.replace(/&/g, '&amp;')
-               .replace(/</g, '&lt;')
-               .replace(/>/g, '&gt;');
-}
